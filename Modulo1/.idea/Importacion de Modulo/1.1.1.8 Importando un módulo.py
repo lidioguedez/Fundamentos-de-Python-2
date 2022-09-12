4b3bfa2589fb126330db4938bd8d7e6c05e414df
abdisoftware <abdisofware@gmail.com>
2022-09-12 20:09:06 -0300

    reinicio

---
+++ Modulo1/.idea/Importacion de Modulo/1.1.1.8 Importando un módulo.py
@@ -0,0 +1,41 @@
+from math import sin, pi
+
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+#############################################
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
+#############################################
+#
+# import module as alias
+# El "module" identifica el nombre del módulo original mientras que el "alias" es el nombre que se desea usar en lugar del original.
+# Nota: as es una palabra clave reservada.
+#
+##############################################
